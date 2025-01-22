76c138d78659a8fd2cc5b8543d91402ab81be813
hyedi3x <hyedi3x@naver.com>
2025-01-22 20:38:27 +0900

    [fix] deleteInfo.js
    - 주석 수정

--- mvc_jsp_movie/src/main/webapp/resources/js/customer/myInfo/deleteInfo/deleteInfo.js
+++ mvc_jsp_movie/src/main/webapp/resources/js/customer/myInfo/deleteInfo/deleteInfo.js
@@ -5,7 +5,7 @@ function deleteGuide(path) {
 	let url = path + "/deleteGuide.do";
 	
 	// window.open( ) :  웹브라우저에서 새창(팝업창)을 여는 함수 
-	// open() 함수의 속성들 | 1. 팝업 주소 : url(위에 선언한 변수 사용), 2. 새로 열리는 창(팝업창)의 이름을 지정 : "confirm" 
+	// open() 함수의 속성들 | 1. 팝업 주소 : url(위에 선언한 변수 사용), 2. 새로 열리는 창(팝업창)의 이름을 지정 : "deleteGuide" 
 	// , 3. 팝업창 설정 : "menubar=no" : 메뉴바 없이 창 생성, 창 위치 조정: left=900, top=250
 	window.open(url, "deleteGuide", "menubar=no, width=400, height=400, left=900, top=250");  
 }
